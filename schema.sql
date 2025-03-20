5293e814339ae266526b168a73801cbaf31ec7b5
Pjdur <pjdurable@gmail.com>
2025-03-20 20:37:45 +0000

    Add packages table to schema.sql and create .env.example file

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+
+CREATE TABLE packages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    package_name TEXT NOT NULL,
+    package_version TEXT NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username)
+);
